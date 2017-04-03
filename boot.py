05a3835611cda5414460ca6a5a79796325455859
tomhermanek@gmail.com <tomhermanek@gmail.com>
2017-04-03 16:51:02 +0200

    Refactored booting mechanism

--- boot.py
+++ boot.py
@@ -1,14 +1,25 @@
 from serial_connection import SlipListener, SlipSender, ContikiBootEvent
+from utils.configuration_loader import ConfigurationLoader
 from data import Data
+import configparser
+import os
 
 
 class Boot(object):
-    def __init__(self):
-        self._data = Data()
-        self._slip_sender = SlipSender('/dev/ttyUSB0')
-        self._slip_listener = SlipListener('/dev/ttyUSB0', self._data)
+    _pwd = os.getcwd()
 
+    def __init__(self):
+        self._load_config()
+        self._load_services()
         self._boot_event_subscribers()
+
+    def _load_config(self):
+        self.configLoader = ConfigurationLoader(configparser.ConfigParser())
+
+    def _load_services(self):
+        self._data = Data(self.configLoader.read_configuration("{0}/configuration/configuration.conf".format(self._pwd)))
+        self._slip_sender = SlipSender(self._data.get_configuration()['serial']['device'])
+        self._slip_listener = SlipListener(self._data.get_configuration()['serial']['device'], self._data)
 
     def _boot_event_subscribers(self):
         self._slip_listener.get_input_parser().subscribe_event(ContikiBootEvent, self._slip_sender)
