d43f6494893e8e45c3522afe10d3a809574e1bb2
tomhermanek@gmail.com <tomhermanek@gmail.com>
2017-04-03 16:50:41 +0200

    added configuration handler into project

--- data.py
+++ data.py
@@ -1,11 +1,15 @@
 
 
 class Data:
-    def __init__(self):
+    def __init__(self, configuration):
         self._src_ip = None
+        self._configuration = configuration
 
     def set_src_ip(self, src_ip):
         self._src_ip = src_ip
 
     def get_src_ip(self):
         return self._src_ip
+
+    def get_configuration(self):
+        return self._configuration
---
+++ utils/configuration_loader.py
@@ -0,0 +1,27 @@
+import configparser
+
+
+class ConfigurationLoader:
+    def __init__(self, conf_parser: configparser):
+        self.confParser = conf_parser
+
+    """
+    Function loads configuration
+    """
+    def read_configuration(self, config_file: str) -> dict:
+        self.confParser.read(config_file)
+        read_config = {
+            "border-router": {},
+            "serial": {},
+            "metrics": {}
+        }
+        for section in self.confParser.sections():
+            if section == 'border-router':
+                read_config[section]['ipv6'] = self.confParser[section]['ipv6']
+            elif section == 'serial':
+                read_config[section]['device'] = self.confParser[section]['device']
+            elif section == 'metrics':
+                read_config[section]['en'] = self.confParser[section]['en']
+                read_config[section]['bw'] = self.confParser[section]['bw']
+                read_config[section]['etx'] = self.confParser[section]['etx']
+        return read_config
